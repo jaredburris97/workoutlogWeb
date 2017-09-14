d2d9584525c23426002966cac569b8a004d766c4
Jared Burris <jaredburris97@gmail.com>
2017-09-14 13:05:49 -0400

    Added files for deployment

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-// $(document).ready(function(){	//anonymous function// When the html doc has loaded run all code
-// 	$("#testAPI").on("click", function(){			// When the #testAPI ID is click
-// 		console.log("It is working! Holy cow!");	// Print to the console "it works..etc"
-// 	});
-
-// 	var test = $.ajax({									// Assigns the ajax request to test
-// 		type: "GET",									// Request for "GETTING" data
-// 		url: "http://localhost:3000/api/test"			// Location of data
-// 	})
-// 	.done(function(data){								// When "test", the ajax call, is run
-// 		console.log(data);								// Print the ajax call's data to the console
-// 	})
-// 	.fail(function(){									// If the ajax call request fails,
-// 		console.log("Oh no! No data! Epic fail!")		// Print to the console "Oh no!"
-// 	});
-// });
-
 $(function(){
 
 	var WorkoutLog = (function($, undefined) {
-		var API_BASE = "http://localhost:3000/api/";
+		var API_BASE = "https://workoutlogapp.herokuapp.com/";
 		var userDefinitions = [];
 
 		var setAuthHeader = function(sessionToken) {
